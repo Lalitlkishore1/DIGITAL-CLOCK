d62b5285fba594e041db461a45460cf2b8492a81
LALIT KISHORE J <jeevatham@gmail.com>
2026-04-01 15:16:48 +0530

    script.js

---
+++ script.js
@@ -0,0 +1,20 @@
+function showTime() {
+
+    var date = new Date();
+
+    var hours = date.getHours();
+    var minutes = date.getMinutes();
+    var seconds = date.getSeconds();
+
+    if(hours < 10) hours = "0" + hours;
+    if(minutes < 10) minutes = "0" + minutes;
+    if(seconds < 10) seconds = "0" + seconds;
+
+    var time = hours + ":" + minutes + ":" + seconds;
+
+    document.getElementById("clock").innerHTML = time;
+}
+
+setInterval(showTime, 1000);
+
+showTime();
